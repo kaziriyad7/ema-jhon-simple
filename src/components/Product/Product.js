6bf741d7d1985f14ceb6c820dae8bec5795efcde
kazi riyad <kazimohiuddinriyad77@gmail.com>
2021-03-09 11:37:32 +0600

    some components modify and style

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,7 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 const Product = (props) => {
-    console.log(props.product);
+    // console.log(props);
+
     const {img, name, seller, price, stock} = props.product;
     return (
         <div className="product">
@@ -17,7 +18,7 @@ const Product = (props) => {
                 <p>${price}</p>
                 <br/>
                 <p><small>Only {stock} Left in stock - Order Soon</small></p>
-                <button className="main-button"><FontAwesomeIcon icon={faShoppingCart} />Add Me Cart</button>    
+                <button className="main-button" onClick = {() => props.handleAddProduct(props.product)}><FontAwesomeIcon icon={faShoppingCart} />Add Me Cart</button>    
             </div>
             
         </div>
